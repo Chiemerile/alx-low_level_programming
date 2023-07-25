a1132d1ea21122cf9eefba9b626ee4f47de75f43
Chiemerile <meril.igwe@gmail.com>
2023-07-25 00:43:52 -0700

    0x03-debugging 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - makes infinite loop
+ * main - makes infint loop
  * Return: 0
  */
 
@@ -10,13 +10,15 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 
-/*while (i < 10)*/
-/*{ */
-/*putchar(i);*/
-/*}*/
+	/*while (i < 10)*/
+	/*{*/
+		/*putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * largest_number - returns largest out of 3 num
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
+ * largest_number - will return the largst out of 3 nums
+ * @a: 1st int
+ * @b: 2nd int
+ * @c: 3rd int
+ * Return: the largst num
  */
 
 int largest_number(int a, int b, int c)
@@ -22,12 +22,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-		else if (b > c)
+	else if (b > c)
 	{
-
 		largest = b;
 	}
-		else
+	else
 	{
 		largest = c;
 	}
